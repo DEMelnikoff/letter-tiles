2e0cfbfda503e26ab3b5cca1384892a302616dba
David E Melnikoff <davidemelnikoff@gmail.com>
2025-08-11 19:53:24 -0700

    Update exp.js

--- js/exp.js
+++ js/exp.js
@@ -528,7 +528,7 @@ const exp = (function() {
         for (let i = 1; i <= 24; i++) {
             if (i !== 11) {
                 stimIdxArray.push(i);
-                let latency = 30 * (i - 1) + 1700;
+                let latency = 30 * (i - 1) + 2500;
                 latencyArray.push(latency);
             };
         };
